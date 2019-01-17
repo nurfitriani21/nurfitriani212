f7fefcd2fec967abe8a14c539b8c5ecc11d257ce
nurfitriani21 <46755888+nurfitriani21@users.noreply.github.com>
2019-01-17 08:31:24 +0800

    Add files via upload

---
+++ mahasiswa.php
@@ -0,0 +1,14 @@
+	<table width = "2000 px" border = "1">
+		<tr>
+			<td> Nama </td>
+			<td> : Nur Fitriani </td>
+		</tr>
+		<tr>
+			<td> NIM </td>
+			<td> : 215280036 </td>
+		</tr>
+		<tr>
+			<td> Kelas </td>
+			<td> : A </td>
+		</tr>
+	</table>
